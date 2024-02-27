317f4a17b4f27a99076a6fe67723550cdd87d894
Ivan-Daniel-Beltran-Santiago <starfoxfan950@hotmail.com>
2024-02-27 14:36:24 -0700

    Pequeños avances en el submenú de Etiquetas

--- client/src/app/components/tag-manager/tag-manager.component.ts
+++ client/src/app/components/tag-manager/tag-manager.component.ts
@@ -3,12 +3,21 @@ import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-tag-manager',
   templateUrl: './tag-manager.component.html',
-  styleUrls: ['./tag-manager.component.css']
+  styleUrls: ['./tag-manager.component.css'],
 })
 export class TagManagerComponent {
   @Output() close: EventEmitter<void> = new EventEmitter<void>();
+  selectedTab: string = 'Crear etiqueta'; // Inicialmente seleccionado
 
   closeTagManager() {
     this.close.emit();
+  }
+
+  selectTab(tabName: string) {
+    this.selectedTab = tabName;
+  }
+
+  isTabSelected(tabName: string): boolean {
+    return this.selectedTab === tabName;
   }
 }
