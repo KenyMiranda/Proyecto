77ab75f80cc1b0cadedc85359f9d63ef2a4fb36d
Ivan-Daniel-Beltran-Santiago <starfoxfan950@hotmail.com>
2024-03-15 21:12:44 -0700

    Ahora se muestran todas las etiquetas de forma dinámica en el primer selector de Editar Etiqueta

--- client/src/app/components/tag-manager/tag-manager-edit-tag/tag-manager-edit-tag.component.ts
+++ client/src/app/components/tag-manager/tag-manager-edit-tag/tag-manager-edit-tag.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TagManagerService } from 'src/app/services/TagManager/tag-manager.service';
 
 @Component({
   selector: 'app-tag-manager-edit-tag',
@@ -9,7 +10,17 @@ export class TagManagerEditTagComponent {
   selectedCategory: string = '';
   optionSelected: boolean = false;
   selectedCourse: string = '';
+  tags: string[] = [];
 
+  constructor(private tagManagerService: TagManagerService) {}
+
+  ngOnInit(): void {
+    // Llama al servicio para obtener todas las etiquetas
+    this.tagManagerService.getTags().subscribe(tags => {
+      this.tags = tags;
+    });
+  }
+  
   onCategoryChange(event: any) {
     this.selectedCategory = event.target.value;
     // Reset selected course when changing category
